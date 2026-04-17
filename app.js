aad32831acec3cff6d278f27fca0f0a36882f1b0
Amy Trice <amylizt@gmail.com>
2026-04-17 17:30:18 -0400

    commit deployment files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
