50e9a6468f7e24e56e1b9cce658099da6c3a2fd4
Amy Trice <amylizt@gmail.com>
2026-05-16 16:48:26 -0400

    updates to pictures

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3002;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'welcome.html'));
 });
 
 app.listen(PORT, () => {
